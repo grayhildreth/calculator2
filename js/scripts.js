388520ff0588303bb4fa4a3f002730fc6e5fe2f9
Eric Krause and Gray Hildreth <Guest@epicodus-79.local>
2016-06-01 19:45:31 -0700

    It will add subtraction, multiplication, and division output to interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var add = function(number1, number2) {
   return number1 + number2;
 };
-var subtact = function(number1, number2) {
+var subtract = function(number1, number2) {
   return number1 - number2;
 };
 var multiply = function(number1, number2) {
@@ -18,5 +18,35 @@ $(document).ready(function() {
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
     $("#output").text(result);
+  });
+});
+
+  $(document).ready(function() {
+    $("form#subtract").submit(function(event) {
+      event.preventDefault();
+      var number1 = parseInt($("#subtract1").val());
+      var number2 = parseInt($("#subtract2").val());
+      var result = subtract(number1, number2);
+      $("#output").text(result);
+  });
+});
+
+    $(document).ready(function() {
+      $("form#multiply").submit(function(event) {
+        event.preventDefault();
+        var number1 = parseInt($("#multiply1").val());
+        var number2 = parseInt($("#multiply2").val());
+        var result = multiply(number1, number2);
+        $("#output").text(result);
+  });
+});
+
+    $(document).ready(function() {
+      $("form#divide").submit(function(event) {
+        event.preventDefault();
+        var number1 = parseInt($("#divide1").val());
+        var number2 = parseInt($("#divide2").val());
+        var result = divide(number1, number2);
+        $("#output").text(result);
   });
 });
